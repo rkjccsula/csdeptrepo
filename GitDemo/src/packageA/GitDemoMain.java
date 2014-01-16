1cecc04101393919e9cdea3b6c4b3be3e31c2ef9
rkjc <richardkjcross@gmail.com>
2014-01-16 05:26:15 -0800

    first commit set up project

---
+++ GitDemo/src/packageA/GitDemoMain.java
@@ -0,0 +1,11 @@
+package packageA;
+
+public class GitDemoMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello ");
+		System.out.println("Git Demo");
+	}
+
+}
